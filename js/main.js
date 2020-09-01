57e4d911c58e1c6c9101d420abd5fc106d412528
valerio <valerio.deprosperi@gmail.com>
2020-09-01 16:25:07 +0200

    problemi js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ console.log(hamburger);
 // creo var hamburger menu e seleziono hamburg menu
 var hamburger_menu = $(".hamburger-menu");
 console.log(hamburger_menu);
+
+// creo funzione per mostrare testo Menu
+hamburger.click(
+  function(){
+    hamburger_menu.show(slow);
+  }
+);

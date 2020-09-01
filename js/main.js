abd822a13cfa88d66b8e5a29daf846c1da29b830
valerio <valerio.deprosperi@gmail.com>
2020-09-01 16:10:59 +0200

    creato var hamburger e hamburger_menu e selezionato elementi html

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// creo var hamburger e seleziono 'a' di header right
+var hamburger = $(".header-right > a");
+console.log(hamburger);
+// creo var hamburger menu e seleziono hamburg menu
+var hamburger_menu = $(".hamburger-menu");
+console.log(hamburger_menu);
